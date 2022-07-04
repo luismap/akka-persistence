3a1432875007b69b35d10e95b31ab25b404c3bb2
Luis Perez <luisperez@luis.arcia-MBP16>
2022-07-04 11:05:59 +0200

    [feat] about persistence part 2

--- src/main/scala/persistence/PersistenctActors.scala
+++ src/main/scala/persistence/PersistenctActors.scala
@@ -1,8 +1,9 @@
 package persistence
 
-import akka.actor.{ActorLogging, ActorSystem, Props}
+import akka.actor.{ActorLogging, ActorSystem, Kill, PoisonPill, Props, actorRef2Scala}
 import akka.persistence.PersistentActor
 import akka.persistence.cassandra.query.UUIDPersistentRepr
+import akka.stream.Supervision.Stop
 
 import java.util.{Date, UUID}
 import scala.concurrent.duration.DurationInt
@@ -14,14 +15,25 @@ object PersistenctActors extends App {
    * scenario
    * Account keeps track of invoice.
    * - sum total per invoice
+   * about persitence Actor
+   * - handle shutdown logic yourself
+   *    because now the message will be handle by the persistence actor logic in the same mailbox
+   *
    */
   sealed trait InvoiceCommands
+
   case class Invoice(recipient: String, date: Date, amount: Int) extends InvoiceCommands
 
+  case class InvoiceBulk(invoices: List[Invoice]) extends InvoiceCommands
+
   sealed trait InvoiceEvents
+
   case class InvoiceRecorded(id: UUID, recipient: String, date: Date, amount: Int) extends InvoiceEvents
 
-  class Accountant extends PersistentActor with ActorLogging {
+  sealed trait SpecialCommands
+  object Shutdown extends SpecialCommands
+
+  class Accountant(id: String) extends PersistentActor with ActorLogging {
     var latestInvoiceId = UUID.randomUUID()
     var totalAmount = 0
 
@@ -47,12 +59,11 @@ object PersistenctActors extends App {
         log.info(s"[printing] current invoice: $latestInvoiceId with total: $totalAmount")
       case Invoice(recipient, date, amount) =>
         log.info(s"[invoice] $recipient with total $amount")
-        persist(InvoiceRecorded(latestInvoiceId,recipient, date, amount))
-        /**
-         * during the time gap of persist and callback function
-         * persistentActor will stash all received messages to keep consistency
-         */
-        {
+        persist(InvoiceRecorded(latestInvoiceId, recipient, date, amount)) {
+          /**
+           * during the time gap of persist and callback function
+           * persistentActor will stash all received messages to keep consistency
+           */
           e =>
 
             /**
@@ -60,33 +71,72 @@ object PersistenctActors extends App {
              * persist guarantees that not other thread access the actor
              * during the call back
              */
+            //NEVER CALL PERSIST OR PERSISTALL from a future
             latestInvoiceId = UUID.randomUUID()
             totalAmount += amount
             //correctly identify the sender of the message
             //sender() ! "PersistenceAck"
             log.info(s"[persisted] event with amount $amount for a total $totalAmount")
         }
+      case InvoiceBulk(invoices) =>
+        //create events, persist all events, update actor state for each event persisted
+        val events = invoices.map(i => InvoiceRecorded(UUID.randomUUID(), i.recipient, i.date, i.amount))
+        persistAll(events) {
+          e =>
+            latestInvoiceId = e.id
+            totalAmount += e.amount
+            log.info(s"[persisted] event wit amount ${e.amount} total: $totalAmount ")
+        }
+      case Shutdown => context.stop(self)
     }
 
 
     //the id should be unique, and is the one use for recovery
-    override def persistenceId: String = "accountant-01"
+    override def persistenceId: String = id
+
+    override protected def onPersistRejected(cause: Throwable, event: Any, seqNr: Long): Unit = {
+      log.error(s"[persist-rejected] fail to persist $event cause: $cause")
+      super.onPersistRejected(cause, event, seqNr)
+    }
+
+    /**
+     * method call if persisted failed
+     * - actor will be stopped
+     * Best practice: use the backoff pattern
+     *
+     * @param cause
+     * @param event
+     * @param seqNr
+     */
+    override protected def onPersistFailure(cause: Throwable, event: Any, seqNr: Long): Unit = {
+      log.error(s"[persist-failure] failed to persist $event cause: $cause")
+      super.onPersistFailure(cause, event, seqNr)
+    }
+
   }
 
-
   val guardian = ActorSystem("guardian")
-  val accountant = guardian.actorOf(Props[Accountant],"accountant")
+  val accountant = guardian.actorOf(Props(new Accountant("accountant-01")), "accountant01")
+  val accountantShutdown = guardian.actorOf(Props(new Accountant("accountant-02")), "accountant02")
   val scheduler = guardian.scheduler
   implicit val execCtx = guardian.dispatcher
   val rnd = Random
 
- for ( e <- 0 to 10 ) {
-   accountant ! Invoice("Bleak Inc.", new Date(), rnd.nextInt(1000))
-   if (e % 2 == 0) accountant ! "print"
- }
+  for (e <- 0 to 10) accountant ! Invoice("Bleak Inc.", new Date(), rnd.nextInt(1000))
+  for (e <- 0 to 10) accountantShutdown ! Invoice("Bleak Inc.", new Date(), rnd.nextInt(1000))
+
+  accountant ! "print"
+
+  val invoices = for( _ <- 0 to 10 ) yield Invoice("Apple Inc.", new Date(), rnd.nextInt(100))
+
+  accountant ! InvoiceBulk(invoices.toList)
 
 
-  scheduler.scheduleOnce(6 seconds){
+  //testing shutdown behaviour
+  accountant ! Shutdown //correct way, will handle message in stashing
+  accountantShutdown ! Kill //wrong, will avoid stashing
+
+  scheduler.scheduleOnce(6 seconds) {
     guardian.terminate()
   }
 
